03081f9345cec4f60f5e2abd000c60702c378570
David Davidson <g00271860@gmit.ie>
2015-10-09 19:35:06 +0100

    Fix remote code execution #1
    Fixes remote code execution caused by passing unsanitized user inputs to shell_exec.
    
    For example, to execute the `id` command, one could do the following: `url=;id` or `id=;id` or similar.

--- snipe/snipe.php
+++ snipe/snipe.php
@@ -1,8 +1,8 @@
 <?php
 if(isset($_POST["id"]) && isset($_POST["url"]))
 {
-	$theid = $_POST["id"];
-	$theurl = $_POST["url"];
+	$theid = escapeshellarg($_POST["id"]);
+	$theurl = escapeshellarg($_POST["url"]);
 	$cmd = 'python cgi-bin/vpsnipe.py '. $theurl . " " . $theid;
 	$results = shell_exec($cmd);
 	echo "Snipping File <br />";
